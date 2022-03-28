c829f112d9955b545cba310db0df8ac175263712
VitaliiaSos <54575558+VitaliiaSos@users.noreply.github.com>
2022-03-28 13:40:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ fig_product.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_
 # ***************************************
 # Activate the app
 # ***************************************
-#app = dash.Dash(__name__)
+
 
 dash_app = dash.Dash(__name__)
 app = dash_app.server
@@ -62,23 +62,7 @@ dash_app.layout = html.Div(
     children=[
         html.Div(className='row',
                 children=[
-                    # html.Div(className='four columns div-user-controls',
-                    #         children=[
-                    #             html.H2('Sales dashboard'),
-                    #             html.P('Select filters from dropdown'),
-
-                    # html.Div(children="Month", className="menu-title"),
-                    #         dcc.Dropdown(
-                    #             id='drop_month',
-                    #             options=[{'label':selectmonth, 'value':selectmonth} for selectmonth in df_month['monthnames']],
-                    #         ),
-                    # html.Div(children="Year", className="menu-title"),
-                    #         dcc.Dropdown(
-                    #             id='drop_year',
-                    #             options=[{'label':selectyear, 'value':selectyear} for selectyear in df_year]
-                    #         ),
-                    #         ]
-                    # ),
+                   
                     html.Div(className='eight columns div-for-charts bg-grey',
                             children=[
                                 dcc.Graph(id="sales_employee", figure=fig_employee)
@@ -93,70 +77,6 @@ dash_app.layout = html.Div(
             )
         ]
 )
-
-# ***************************************
-# Callbacks
-# ***************************************
-# Sales Employee
-# ***************************************
-
-#@dash_app.callback(Output('sales_employee', 'figure'),
-#              [Input('drop_month', 'value')],
-#              [Input('drop_year', 'value')])
-
-#def update_graph(drop_month, drop_year):
-#    if drop_year:
-#        if drop_month:
-#            # Data i både drop_month og drop_year
-#            order_fig1 = order.loc[(order['orderyear'] == drop_year) & (order['ordermonth'] == drop_month)]
-#        else:
-#            # Data i drop_year. men ikke drop_month
-#            order_fig1 = order.loc[order['orderyear'] == drop_year]
-#    else:
-#        if drop_month:
-#            # Data i drop_month, men ikke drop_year
-#            order_fig1 = order.loc[order['ordermonth'] == drop_month]
-#        else:
-#            # Ingen data - ikke noget valgt
-#            order_fig1 = order
-#        
-#    return {'data':[go.Bar(
-#        x = order_fig1['productname'],
-#        y = order_fig1['total']
-#            )
-#        ]
-#    }
-#
-# ***************************************
-# Sales Products
-# ***************************************
-
-#@dash_app.callback(Output('sales_product', 'figure'),
-#              [Input('drop_month2', 'value')],
-#              [Input('drop_year2', 'value')])
-
-# def update_graph(drop_month, drop_year):
-#     if drop_year:
-#         if drop_month:
-#             # Data i både drop_month og drop_year
-#             order_fig1 = order.loc[(order['orderyear'] == drop_year) & (order['ordermonth'] == drop_month)]
-#         else:
-#             # Data i drop_year. men ikke drop_month
-#             order_fig1 = order.loc[order['orderyear'] == drop_year]
-#     else:
-#         if drop_month:
-#             # Data i drop_month, men ikke drop_year
-#             order_fig1 = order.loc[order['ordermonth'] == drop_month]
-#         else:
-#             # Ingen data - ikke noget valgt
-#             order_fig1 = order
-        
-#     return {'data':[go.Bar(
-#         x = order_fig1['productname'],
-#         y = order_fig1['total']
-#             )
-#         ]
-#     }
 
 
 # ***************************************
